67175b29b4b70bccf0280942d8103ac2f63c45f9
kamila <ksalimzh@gmail.com>
2025-03-13 13:59:34 +0500

    Initial Commit

---
+++ learning_swiftui/learning_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swiftuiApp.swift
+//  learning_swiftui
+//
+//  Created by kamila on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct learning_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
